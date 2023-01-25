93c908f7a57145d899fa127ce0fe7baa571defa6
toyama <shouya.toyama@gmail.com>
2023-01-25 13:14:11 +0000

    zig fmt

--- basics/main.zig
+++ basics/main.zig
@@ -2,12 +2,12 @@ const std = @import("std");
 const module = @import("module.zig");
 
 pub fn main() void {
-  var inferred_constant = @as(i32, 3);
+    var inferred_constant = @as(i32, 3);
 
-  std.debug.print("{d}\n", .{inferred_constant});
+    std.debug.print("{d}\n", .{inferred_constant});
 
-  const a =[5]u8{'h', 'e', 'l', 'l', 'o'};
-  std.debug.print("{d}\n", .{a.len});
+    const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
+    std.debug.print("{d}\n", .{a.len});
 
-  module.greet("Hello");
+    module.greet("Hello");
 }
--- basics/module.zig
+++ basics/module.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn greet(msg: []const u8) void {
-  std.debug.print("{s}!\n", .{msg});
+    std.debug.print("{s}!\n", .{msg});
 }
